8ed7276e1343057b1fb4127c71fea74d2dcb3577
juslin03 <juslin03>
2019-11-01 14:22:46 +0000

    MAJ

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
-class Footer extends React.Component {
+class Footer extends Component {
   render() {
     return (
       <footer>
         <p>
-          Powered by <a href="https://javadart.github.io">Juslin</a>
+          Powered by <a href="https://jjuslin03.github.io">Juslin</a>
         </p>
       </footer>
     );
